b7666e67534bd8c10980744e6fd356a82552292f
Regino Deang <reginodeangjr@gmail.com>
2023-11-16 21:08:27 +0800

    updated q1

--- assignment/q1 updated answer.js
+++ assignment/q1 updated answer.js
@@ -1,39 +1,61 @@
-class BaseSignal {
-    constructor(signalType) {
-        if (new.target === BaseSignal) {
-            throw new Error("Not Allowed");
-        }
-        this.type = signalType;
-    }
+/*
+    Task
+    - Create a "BaseSignal" class to be inherited by "TvSignal", "AirconSignal" and "DoorSignal" class.
+    - In the "BaseSignal" class, throw an error within the constructor to block direct instantiation (see doc-permissions.js for example).
+    - Implement `send` method to print `Sending ${type} signal` in the BaseSignal class.
+    - In the respective child classes, call `super()` with it's type.
+*/
 
-    send() {
-        console.log(`Sending ${this.type} signal`);
+// Task: Add code here
+
+class BaseSignal {
+  constructor() {
+    if (this.constructor.name === "BaseSignal") {
+      throw new Error("This class cannot be instantiated");
     }
+  }
+  send(type) {
+    console.log(`Sending ${type} signal`);
+  }
 }
 
 class TvSignal extends BaseSignal {
-    constructor() {
-        super("TV");
-    }
+  constructor() {
+    super();
+    this.type = "tv";
+  }
+  send() {
+    super.send(this.type);
+  }
 }
 
 class AirconSignal extends BaseSignal {
-    constructor() {
-        super("Aircon");
-    }
+  constructor() {
+    super();
+    this.type = "aircon";
+  }
+  send() {
+    super.send(this.type);
+  }
 }
 
 class DoorSignal extends BaseSignal {
-    constructor() {
-        super("Door");
-    }
+  constructor() {
+    super();
+    this.type = "door";
+  }
+  send() {
+    super.send(this.type);
+  }
 }
 
-const tv = new TvSignal();
-tv.send(); // prints "Sending TV signal"
+var tv = new TvSignal();
+tv.send(); // prints "Sending tv signal"
 
 const door = new DoorSignal();
-door.send(); // prints "Sending Door signal"
+door.send(); // prints "Sending door signal"
 
 const aircon = new AirconSignal();
-aircon.send(); // prints "Sending Aircon signal"
+aircon.send(); // prints "Sending aircon signal"
+
+//const NewSignal = new BaseSignal();
